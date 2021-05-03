f77ae3b651273f810abda3b688255b5ecc61d487
Petra Boldi <petraboldi@Petras-MacBook-Air.local>
2021-05-03 14:16:19 +0200

    second cypress test

--- cypress/integration/bookmarks.spec.js
+++ cypress/integration/bookmarks.spec.js
@@ -8,4 +8,10 @@ describe('Bookmarks page', function(){
         cy.get('#save-button').click()
         cy.contains('www.instagram.com')
     })
+       it('deleting a bookmark', function(){
+        cy.get('#url-input').type('www.instagram.com')
+        cy.get('#save-button').click()
+        cy.get('#delete-0').click()
+        cy.get('url-0').should('not.exist')
+    })
 })
