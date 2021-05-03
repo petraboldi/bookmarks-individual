b88e98a45477f9b766918682f66a5841033f4c06
Petra Boldi <petraboldi@Petras-MacBook-Air.local>
2021-05-03 14:44:35 +0200

    third test case

--- cypress/integration/bookmarks.spec.js
+++ cypress/integration/bookmarks.spec.js
@@ -14,4 +14,12 @@ describe('Bookmarks page', function(){
         cy.get('#delete-0').click()
         cy.get('#url-0').should('not.exist')
     })
+        it('updating a bookmark', function(){
+        cy.get('#url-input').type('www.instagram.com')
+        cy.get('#save-button').click()
+        cy.get('#update-0').click()
+        cy.get('#url-input-update').type('www.facebook.com')
+        cy.get('#update-button').click()
+        cy.contains('www.facebook.com')
+    })
 })
