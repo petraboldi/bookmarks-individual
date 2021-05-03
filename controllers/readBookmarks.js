c83711b06db728681e2dbf081aa4d4b351e48872
Petra Boldi <petraboldi@Petras-MacBook-Air.local>
2021-05-03 12:54:16 +0200

    setting up database

--- controllers/readBookmarks.js
+++ controllers/readBookmarks.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req,res)=>{
-    res.render('./bookmarks')
+const Bookmark = require('../models').Bookmarks
+
+router.get('/', async (req,res)=>{
+    const bookmarks = await Bookmark.findAll()
+    res.render('./bookmarks',{bookmarks:bookmarks})
 })
 
 module.exports = router;
--- db-reset.js
+++ db-reset.js
@@ -2,7 +2,7 @@ const db = require('./models')
 
 const truncateTables = () => {
   console.log('truncating tables')
-  db.Bookmark.destroy({ truncate : true, cascade: true })
+  db.Bookmarks.destroy({ truncate : true, cascade: true })
 }
 
 module.exports = truncateTables
